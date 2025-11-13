754ffdf0065ab6e04428432a7ad6cb36ef8a8fc4
前村海晟 <kaisei1182227@gmail.com>
2025-11-13 15:32:21 +0900

    11/13

--- src/a/Sub.java
+++ src/a/Sub.java
@@ -2,4 +2,35 @@ package a;
 
 public class Sub {
 
+	int price;
+	
+	int id;
+	
+	String name;
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
